9423a5db69a70dbe101235671d636870d599a29d
sarangnath <sarangnadhkayyur@gmail.com>
2024-09-30 11:43:24 +0530

    Modified files

---
+++ src/app/Pages/navbarr/navbarr.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbarr',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbarr.component.html',
+  styleUrl: './navbarr.component.css'
+})
+export class NavbarrComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarrComponent } from "./Pages/navbarr/navbarr.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarrComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
