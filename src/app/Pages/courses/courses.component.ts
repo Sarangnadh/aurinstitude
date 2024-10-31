c3c563b8eb9c4bb0c50de73fadf6ade9d801c3cd
sarangnath <sarangnadhkayyur@gmail.com>
2024-10-31 21:57:16 +0530

    data added

--- src/app/Pages/courses/courses.component.ts
+++ src/app/Pages/courses/courses.component.ts
@@ -10,6 +10,71 @@ import { Router } from '@angular/router';
   styleUrl: './courses.component.css'
 })
 export class CoursesComponent implements OnInit {
+  courses :any[]=[
+    {
+      id:1,
+      courseName:"BSc Interior Design",
+      Duration:"3 Years (6 Semesters)",
+      Eligibility:" 10+2 with a minimum 50% aggregate marks",
+      desaiif:"Value added programs from AIIF"
+    },
+    {
+      id:2,
+      courseName:"Diploma in Interior Design and Space Management",
+      Duration:"14 Months",
+      Eligibility:"10+2 with a minimum 50% aggregate marks ",
+      desaiif:"Value added programs from AIIF"
+    },
+    {
+      id:3,
+      courseName:"B.Des Interior Design",
+      Duration:" 4 Years",
+      Eligibility:" 10+2 with a minimum 50% aggregate marks",
+      desaiif:"Value added programs from AIIF"
+    },
+    {
+      id:4,
+      courseName:"PG Diploma in Interior Designand Space Management",
+      Duration:"18 Months",
+      Eligibility:" A graduate degree in any discipline",
+      desaiif:"Value added programs from AIIF"
+    },
+    {
+      id:5,
+      courseName:"MDes Furniture and Interior Design",
+      Duration:"2 Years(4 Semesters)",
+      Eligibility:"Bachelor's Degree in Design / Fine Arts / Applied Arts / Architecture / Civil Engineering",
+      desaiif:"Value added programs from AIIF"
+    },
+    {
+      id:6,
+      courseName:"BSc Costume Design and Fashion",
+      Duration:"3 Years",
+      Eligibility:"10+2 with a minimum 50% aggregate marks ",
+      desaiif:"Value added programs from AIIF"
+    },
+    {
+      id:7,
+      courseName:"Advanced Diploma in Fashion Design and Boutique Management",
+      Duration:"18 Months",
+      Eligibility:"10+2 with a minimum 50% aggregate marks ",
+      desaiif:"Value added programs from AIIF"
+    },
+    // {
+    //   id:8,
+    //   courseName:"",
+    //   Duration:"",
+    //   Eligibility:" ",
+    //   desaiif:"Value added programs from AIIF"
+    // },
+    // {
+    //   id:9,
+    //   courseName:"",
+    //   Duration:"",
+    //   Eligibility:" ",
+    //   desaiif:"Value added programs from AIIF"
+    // },
+  ]
   constructor(private router: Router) { }
   ngOnInit(): void {
 
